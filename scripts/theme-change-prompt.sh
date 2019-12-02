a4d733aac7b9452e1c4a8393380270d8d1146956
keystroke3 <keystroke33@gmail.com>
2019-12-02 14:48:43 +0300

    A few minor changes made and comments added

--- scripts/theme-change-prompt.sh
+++ scripts/theme-change-prompt.sh
@@ -1,12 +1,45 @@
 #!/bin/bash
 
-SDIR="$HOME/.config/polybar/scripts"
-CURRENT=light
-UPDATE="$SDIR/theme-change.sh $CURRENT"
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#  _                  _             _         _____                     #
+# | |                | |           | |       |____ |                    #
+# | | _____ _   _ ___| |_ _ __ ___ | | _____     / /                    #
+# | |/ / _ \ | | / __| __| '__/ _ \| |/ / _ \    \ \                    #
+# |   <  __/ |_| \__ \ |_| | | (_) |   <  __/.___/ /                    #
+# |_|\_\___|\__, |___/\__|_|  \___/|_|\_\___|\____/                     #
+#            __/ |                                                      #
+#           |___/                                                       #
+#                                                                       #
+# Github: https://github.com/keystroke3                                 #
+# Reddit: u/SiliconRaven                                                #
+# Twitter: @SiliconRaven                                                #
+# License: GPLv3 https://www.gnu.org/licenses/gpl-3.0.en.html           #
+#                                                                       #
+#                                                                       #
+# Useful resources:                                                     #
+# https://github.com/polybar/polybar-scripts                            #
+# https://github.com/davatorium/rofi                                    #
+# https://github.com/jordansissel/xdotool                               #
+# https://github.com/dylanaraps/pywal                                   #
+# https://www.gnu.org/software/sed/manual/sed.html                      #
+#                                                                       #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
+
+SDIR="$HOME/.config/polybar/scripts"
+
+# CURRENT=transparent1
+# UPDATE takes the the current config and place it in theme-change.sh which will then modify
+# the CURRENT value in this file after refreshing Polybar
+CURRENT=transparent1
+UPDATE="$SDIR/theme-change.sh $CURRENT" 
+
+# Get the display dimensions and where pointer is.
 eval "$(xdotool getmouselocation --shell)"
 eval "$(xdotool getdisplaygeometry --shell)"
 
+# Checks the postion of the mouse and determines where the popup should be.
+# If the user's rofi theme has set dimensions and position, the user's configs are maintained
 if [ "$Y" -lt "$((HEIGHT/2))" ]; then
 	YSET=35
 	if [ "$X" -gt "$(((WIDTH*2)/3))" ]; then #Right side
@@ -34,23 +67,63 @@ elif [ "$Y" -gt "$((HEIGHT/2))" ]; then
 fi
 
 position="rofi -sep | -dmenu -i  -location $LOC -xoffset $XSET -yoffset $YSET -hide-scrollbar"
-colors="  Pywal Colors|👕 black|👕 white |👕 red|👕 lime|👕 blue|👕 yellow|👕 aqua|👕 fuchsia|👕 silver|👕 gray|👕 maroon|👕 olive|👕 green|👕 purple|👕 teal|👕 navy|👕 pink"
-theme="🔆 light|🔆 lighter|🌜 dark|🌜 darker"
+colors="  pywal_colors|👕 black|👕 white|👕 red|👕 lime|👕 blue|👕 yellow|👕 aqua|👕 fuchsia|👕 silver|👕 gray|👕 maroon|👕 olive|👕 green|👕 purple|👕 teal|👕 navy|👕 pink"
+pywal_colors=" xcolor0| xcolor1| xcolor2| xcolor3| xcolor4| xcolor5| xcolor6| xcolor7| xcolor8| xcolor9 | xcolor10| xcolor11| xcolor12| xcolor13| xcolor14| xcolor15"
 
-
-select_theme(){
-	
-	case "$MENU4" in
-		*light) $SDIR/theme-change.sh light ;;
-		*lighter) $SDIR/theme-change.sh lighter ;;
-		*dark) $SDIR/theme-change.sh dark ;;
-		*darker) $SDIR/theme-change.sh darker ;;
+# This part sets a user specified Pywal color and makes it the first color.
+select_pywal1(){
+	MENU="$($position -p Pywal_Color -columns 3 -lines 6 -width 30 <<<$pywal_colors)"
+	case "$MENU" in
+		*red) sed -i -e "0,/^color1/ s/color1.*/color1='$\{colors.red}'/" $SDIR/theme-change.sh ;;
+		*xcolor0) sed -i -e "0,/^color1/ s/color1.*/color1='$\{colors.xcolor0}'/" $SDIR/theme-change.sh ;;
+		*xcolor1) sed -i -e "0,/^color1/ s/color1.*/color1='$\{colors.xcolor1}'/" $SDIR/theme-change.sh ;;
+		*xcolor2) sed -i -e "0,/^color1/ s/color1.*/color1='$\{colors.xcolor2}'/" $SDIR/theme-change.sh ;;
+		*xcolor3) sed -i -e "0,/^color1/ s/color1.*/color1='$\{colors.xcolor3}'/" $SDIR/theme-change.sh ;;
+		*xcolor4) sed -i -e "0,/^color1/ s/color1.*/color1='$\{colors.xcolor4}'/" $SDIR/theme-change.sh ;;
+		*xcolor5) sed -i -e "0,/^color1/ s/color1.*/color1='$\{colors.xcolor5}'/" $SDIR/theme-change.sh ;;
+		*xcolor6) sed -i -e "0,/^color1/ s/color1.*/color1='$\{colors.xcolor6}'/" $SDIR/theme-change.sh ;;
+		*xcolor7) sed -i -e "0,/^color1/ s/color1.*/color1='$\{colors.xcolor7}'/" $SDIR/theme-change.sh ;;
+		*xcolor8) sed -i -e "0,/^color1/ s/color1.*/color1='$\{colors.xcolor8}'/" $SDIR/theme-change.sh ;;
+		*xcolor9) sed -i -e "0,/^color1/ s/color1.*/color1='$\{colors.xcolor9}'/" $SDIR/theme-change.sh ;;
+		*xcolor10) sed -i -e "0,/^color1/ s/color1.*/color1='$\{colors.xcolor10}'/" $SDIR/theme-change.sh ;;
+		*xcolor11) sed -i -e "0,/^color1/ s/color1.*/color1='$\{colors.xcolor11}'/" $SDIR/theme-change.sh ;;
+		*xcolor12) sed -i -e "0,/^color1/ s/color1.*/color1='$\{colors.xcolor12}'/" $SDIR/theme-change.sh ;;
+		*xcolor13) sed -i -e "0,/^color1/ s/color1.*/color1='$\{colors.xcolor13}'/" $SDIR/theme-change.sh ;;
+		*xcolor14) sed -i -e "0,/^color1/ s/color1.*/color1='$\{colors.xcolor14}'/" $SDIR/theme-change.sh ;;
+		*xcolor15) sed -i -e "0,/^color1/ s/color1.*/color1='$\{colors.xcolor15}'/" $SDIR/theme-change.sh ;;		
 	esac
+	$UPDATE
 }
 
+# This does the same thing as select_pywal1 but with the second color
+select_pywal2(){
+	MENU="$($position -p Pywal_Color -columns 3 -lines 6 -width 30 <<<$pywal_colors)"
+	case "$MENU" in
+		*xcolor0) sed -i -e "0,/^color2/ s/color2.*/color2='$\{colors.xcolor0}'/" $SDIR/theme-change.sh ;;
+		*xcolor1) sed -i -e "0,/^color2/ s/color2.*/color2='$\{colors.xcolor1}'/" $SDIR/theme-change.sh ;;
+		*xcolor2) sed -i -e "0,/^color2/ s/color2.*/color2='$\{colors.xcolor2}'/" $SDIR/theme-change.sh ;;
+		*xcolor3) sed -i -e "0,/^color2/ s/color2.*/color2='$\{colors.xcolor3}'/" $SDIR/theme-change.sh ;;
+		*xcolor4) sed -i -e "0,/^color2/ s/color2.*/color2='$\{colors.xcolor4}'/" $SDIR/theme-change.sh ;;
+		*xcolor5) sed -i -e "0,/^color2/ s/color2.*/color2='$\{colors.xcolor5}'/" $SDIR/theme-change.sh ;;
+		*xcolor6) sed -i -e "0,/^color2/ s/color2.*/color2='$\{colors.xcolor6}'/" $SDIR/theme-change.sh ;;
+		*xcolor7) sed -i -e "0,/^color2/ s/color2.*/color2='$\{colors.xcolor7}'/" $SDIR/theme-change.sh ;;
+		*xcolor8) sed -i -e "0,/^color2/ s/color2.*/color2='$\{colors.xcolor8}'/" $SDIR/theme-change.sh ;;
+		*xcolor9) sed -i -e "0,/^color2/ s/color2.*/color2='$\{colors.xcolor9}'/" $SDIR/theme-change.sh ;;
+		*xcolor10) sed -i -e "0,/^color2/ s/color2.*/color2='$\{colors.xcolor10}'/" $SDIR/theme-change.sh ;;
+		*xcolor11) sed -i -e "0,/^color2/ s/color2.*/color2='$\{colors.xcolor11}'/" $SDIR/theme-change.sh ;;
+		*xcolor12) sed -i -e "0,/^color2/ s/color2.*/color2='$\{colors.xcolor12}'/" $SDIR/theme-change.sh ;;
+		*xcolor13) sed -i -e "0,/^color2/ s/color2.*/color2='$\{colors.xcolor13}'/" $SDIR/theme-change.sh ;;
+		*xcolor14) sed -i -e "0,/^color2/ s/color2.*/color2='$\{colors.xcolor14}'/" $SDIR/theme-change.sh ;;
+		*xcolor15) sed -i -e "0,/^color2/ s/color2.*/color2='$\{colors.xcolor15}'/" $SDIR/theme-change.sh ;;		
+	esac
+	$UPDATE
+}
+
+# Same as the above but with prespecified colors
 select_color1(){
-	MENU3="$($position -p Light_Color -columns 3 -lines 6 -width 30 <<<$colors)"
-	case "$MENU3" in
+	MENU="$($position -p Light_Color -columns 3 -lines 6 -width 30 <<<$colors)"
+	case "$MENU" in
+		*pywal_colors) select_pywal1 ;;
 		*black) sed -i -e "0,/^color1/ s/color1.*/color1='$\{colors.black}'/" $SDIR/theme-change.sh ;;
 		*white) sed -i -e "0,/^color1/ s/color1.*/color1='$\{colors.white}'/" $SDIR/theme-change.sh ;;
 		*red) sed -i -e "0,/^color1/ s/color1.*/color1='$\{colors.red}'/" $SDIR/theme-change.sh ;;
@@ -67,14 +140,15 @@ select_color1(){
 		*purple) sed -i -e "0,/^color1/ s/color1.*/color1='$\{colors.purple}'/" $SDIR/theme-change.sh ;;
 		*teal) sed -i -e "0,/^color1/ s/color1.*/color1='$\{colors.teal}'/" $SDIR/theme-change.sh ;;
 		*navy) sed -i -e "0,/^color1/ s/color1.*/color1='$\{colors.navy}'/" $SDIR/theme-change.sh ;;
-		*pink) sed -i -e "0,/^color1/ s/color1.*/color1='$\{colors.pink}'/" $SDIR/theme-change.sh ;;
+		*pink) echo $MENU3;sed -i -e "0,/^color1/ s/color1.*/color1='$\{colors.pink}'/" $SDIR/theme-change.sh ;;
 	esac
 	$UPDATE
 }
 
 select_color2(){
-	MENU2="$($position -p Dark_Color -columns 3 -lines 6 -width 40 <<<$colors)"
-	case "$MENU2" in
+	MENU="$($position -p Dark_Color -columns 3 -lines 6 -width 30 <<<$colors)"
+	case "$MENU" in
+		*pywal_colors) select_pywal2 ;;
 		*black) sed -i -e "0,/^color2/ s/color2.*/color2='$\{colors.black}'/" $SDIR/theme-change.sh ;;
 		*white) sed -i -e "0,/^color2/ s/color2.*/color2='$\{colors.white}'/" $SDIR/theme-change.sh ;;
 		*red) sed -i -e "0,/^color2/ s/color2.*/color2='$\{colors.red}'/" $SDIR/theme-change.sh ;;
@@ -96,30 +170,34 @@ select_color2(){
 	$UPDATE
 }
 
+
+# Prompt for the user to choose.
 popup(){
-	MENU1="$($position -columns 1 -width 20 -lines 3 <<<"👕 First_Color|👕 Second_Color")"
-	case "$MENU1" in
+	MENU="$($position -columns 1 -width 20 -lines 3 <<<"👕 First_Color|👕 Second_Color")"
+	case "$MENU" in
 		*First_Color) select_color1 ;;
 		*Second_Color) select_color2 ;;
 	esac
 }
 
+
+# This part waits for instructions from the polybar module. When --show is supplied,
+# it shows the selection menu. When --switch is supplied, it cycles through the varous
+# configurations 
 case "$1" in
 	--show)
 	popup ;;
 	--switch)
 	case "$CURRENT" in
-	*lighter) MENU4=dark;select_theme ;;
-	*dark) MENU4=darker; select_theme ;;
-	*darker) MENU4=light; select_theme ;;
-	*light) MENU4=lighter; select_theme ;;
+	*white1) $SDIR/theme-change.sh white2 ;;
+	*white2) $SDIR/theme-change.sh transparent1 ;;
+	*transparent1) $SDIR/theme-change.sh transparent2 ;;
+	*transparent2) $SDIR/theme-change.sh black1 ;;
+	*black1) $SDIR/theme-change.sh black2 ;;
+	*black2) $SDIR/theme-change.sh white1 ;;
 	esac
 	;;
-	*)
-	echo "👕"
+	*) echo "running" ;;
 esac
-
-	
-# -location 3 -columns 4  -width 50 -hide-scrollbar -line-padding 4 -padding 20 -lines 10
 
 
--- scripts/theme-change.sh
+++ scripts/theme-change.sh
@@ -1,52 +1,104 @@
 #!/bin/bash
 
-# sed "/foo/{s/foo/bar/1;q}" file
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#  _                  _             _         _____                     #
+# | |                | |           | |       |____ |                    #
+# | | _____ _   _ ___| |_ _ __ ___ | | _____     / /                    #
+# | |/ / _ \ | | / __| __| '__/ _ \| |/ / _ \    \ \                    #
+# |   <  __/ |_| \__ \ |_| | | (_) |   <  __/.___/ /                    #
+# |_|\_\___|\__, |___/\__|_|  \___/|_|\_\___|\____/                     #
+#            __/ |                                                      #
+#           |___/                                                       #
+#                                                                       #
+# Github: https://github.com/keystroke3                                 #
+# Reddit: u/SiliconRaven                                                #
+# Twitter: @SiliconRaven                                                #
+# License: GPLv3 https://www.gnu.org/licenses/gpl-3.0.en.html           #
+#                                                                       #
+#                                                                       #
+# Useful resources:                                                     #
+# https://github.com/polybar/polybar-scripts                            #
+# https://github.com/davatorium/rofi                                    #
+# https://github.com/jordansissel/xdotool                               #
+# https://github.com/dylanaraps/pywal                                   #
+# https://www.gnu.org/software/sed/manual/sed.html                      #
+#                                                                       #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+
 
 PDIR="$HOME/.config/polybar/"
 LAUNCH="polybar-msg cmd restart"
 
-color1='${colors.pink}'
+#These are the two colors to be alternated
+color1='${colors.xcolor15}'
 color2='${colors.black}'
 
-lighter(){
+
+white1(){
     sed -i -e "0,/^shade1/ s/shade1.*/shade1 = $color1/" $PDIR/bubbles.ini
     sed -i -e "0,/^shade2/ s/shade2.*/shade2 = $color2/" $PDIR/bubbles.ini
     sed -i -e "0,/^background/ s/background.*/background = $\{colors.white\}/" $PDIR/bubbles.ini
-    sed -i -e "0,/^CURRENT/ s/CURRENT.*/CURRENT=lighter/" $PDIR/scripts/theme-change-prompt.sh
+    sed -i -e "0,/^CURRENT/ s/CURRENT.*/CURRENT=white1/" $PDIR/scripts/theme-change-prompt.sh
 }
 
-light(){
+white2(){
+    sed -i -e "0,/^shade1/ s/shade1.*/shade1 = $color2/" $PDIR/bubbles.ini
+    sed -i -e "0,/^shade2/ s/shade2.*/shade2 = $color1/" $PDIR/bubbles.ini
+    sed -i -e "0,/^background/ s/background.*/background = $\{colors.white\}/" $PDIR/bubbles.ini
+    sed -i -e "0,/^CURRENT/ s/CURRENT.*/CURRENT=white2/" $PDIR/scripts/theme-change-prompt.sh
+}
+
+
+transparent1(){
     sed -i -e "0,/^shade1/ s/shade1.*/shade1 = $color1/" $PDIR/bubbles.ini
     sed -i -e "0,/^shade2/ s/shade2.*/shade2 = $color2/" $PDIR/bubbles.ini
     sed -i -e "0,/^background/ s/background.*/background = $\{colors.transparent\}/" $PDIR/bubbles.ini
-    sed -i -e "0,/^CURRENT/ s/CURRENT.*/CURRENT=light/" $PDIR/scripts/theme-change-prompt.sh
+    sed -i -e "0,/^CURRENT/ s/CURRENT.*/CURRENT=transparent1/" $PDIR/scripts/theme-change-prompt.sh
 }
 
-dark(){
+transparent2(){
     sed -i -e "0,/^shade1/ s/shade1.*/shade1 = $color2/" $PDIR/bubbles.ini
     sed -i -e "0,/^shade2/ s/shade2.*/shade2 = $color1/" $PDIR/bubbles.ini
     sed -i -e "0,/^background/ s/background.*/background = $\{colors.transparent\}/" $PDIR/bubbles.ini
-    sed -i -e "0,/^CURRENT/ s/CURRENT.*/CURRENT=dark/" $PDIR/scripts/theme-change-prompt.sh
+    sed -i -e "0,/^CURRENT/ s/CURRENT.*/CURRENT=transparent2/" $PDIR/scripts/theme-change-prompt.sh
 
 }
 
-darker(){
+black1(){
     sed -i -e "0,/^shade1/ s/shade1.*/shade1 = $color1/" $PDIR/bubbles.ini
     sed -i -e "0,/^shade2/ s/shade2.*/shade2 = $color2/" $PDIR/bubbles.ini
     sed -i -e "0,/^background/ s/background.*/background = $\{colors.black\}/" $PDIR/bubbles.ini
-    sed -i -e "0,/^CURRENT/ s/CURRENT.*/CURRENT=darker/" $PDIR/scripts/theme-change-prompt.sh
+    sed -i -e "0,/^CURRENT/ s/CURRENT.*/CURRENT=black1/" $PDIR/scripts/theme-change-prompt.sh
 }
 
-if [[ $1 = "dark" ]]; then
-    dark   
-elif [[ $1 = "light" ]]; then
-    light
-elif [[ $1 = "darker" ]]; then
-    darker
-elif [[ $1 = "lighter" ]]; then
-    lighter
+black2(){
+    sed -i -e "0,/^shade1/ s/shade1.*/shade1 = $color2/" $PDIR/bubbles.ini
+    sed -i -e "0,/^shade2/ s/shade2.*/shade2 = $color1/" $PDIR/bubbles.ini
+    sed -i -e "0,/^background/ s/background.*/background = $\{colors.black\}/" $PDIR/bubbles.ini
+    sed -i -e "0,/^CURRENT/ s/CURRENT.*/CURRENT=black2/" $PDIR/scripts/theme-change-prompt.sh
+}
+
+if [[ $1 = "white1" ]]; then
+    white1  
+elif [[ $1 = "white2" ]]; then
+    white2
+elif [[ $1 = "transparent1" ]]; then
+    transparent1
+elif [[ $1 = "transparent2" ]]; then
+    transparent2
+elif [[ $1 = "black1" ]]; then
+    black1
+elif [[ $1 = "black2" ]]; then
+    black2
 else
 echo "optins are:
--dark   -darker
--light  -lighter"
+-white1         -white2
+-dark1          -dark2
+-transparent1   -transparent2"
 fi
+
+
+
+
+# EOF
