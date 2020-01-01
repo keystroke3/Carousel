073f127611ad3b3348b8c86e91aa4959eb9db5a5
keystroke3 <keystroke33@gmail.com>
2020-01-01 21:08:56 +0300

    added new theme stripe

--- launch.sh
+++ launch.sh
@@ -1,23 +1,9 @@
-#!/usr/bin/env bash
 
-## Terminate already running bar instances
-#killall -q polybar
-
-## Wait until the processes have been shut down
-#while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-#for m in $(polybar --list-monitors | cut -d":" -f1); do
-#	WIRELESS=$(ls /sys/class/net/ | grep ^wl | awk 'NR==1{print $1}') MONITOR=$m polybar --reload mainbar-bspwm -c ~/.config/polybar/config &
-#done
-
-#echo "Bars launched..."
-
-#===========================================================================#
 
 
 # Add this to your wm startup file.
 
- Terminate already running bar instances
+#Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
@@ -27,7 +13,7 @@ killall -q polybar
 
 if type "xrandr" > /dev/null; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload mainbar-bspwm -c ~/.config/polybar/bubbles.ini &
+    MONITOR=$m polybar --reload mainbar-bspwm -c ~/.config/polybar/stripes.ini &
   done
 fi
 
@@ -44,4 +30,20 @@ fi
 #echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 #polybar mainbar-bspwm >>/tmp/polybar1.log 2>&1 &
 
+#===========================================================================#
 
+#!/usr/bin/env bash
+
+## Terminate already running bar instances
+#killall -q polybar
+
+## Wait until the processes have been shut down
+#while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+#for m in $(polybar --list-monitors | cut -d":" -f1); do
+#	WIRELESS=$(ls /sys/class/net/ | grep ^wl | awk 'NR==1{print $1}') MONITOR=$m polybar --reload mainbar-bspwm -c ~/.config/polybar/config &
+#done
+
+#echo "Bars launched..."
+
+#===========================================================================#
