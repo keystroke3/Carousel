e91881de05551fa35951576a6fae54ee5c9d02d0
keystroke3 <keystroke33@gmail.com>
2021-04-02 13:17:29 +0300

    regular update

--- launch.sh
+++ launch.sh
@@ -1,14 +1,6 @@
-# killall -q polybar
-# if type "xrandr" > /dev/null; then
-#   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-#     MONITOR=$m
-#     polybar --reload main -c ~/Carousel/config.ini &
-#     polybar --reload bottom -c ~/Carousel/config.ini &
-#   done
-# fi
 killall -q polybar
 
 polybar --reload main -c ~/Carousel/config.ini &
 polybar --reload bottom -c ~/Carousel/config.ini &
 polybar --reload main -c ~/Carousel/config-hdmi.ini &
-polybar --reload bottom -c ~/Carousel/config-hdmi.ini &
+
